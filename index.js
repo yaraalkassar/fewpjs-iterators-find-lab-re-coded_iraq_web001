14d15137344096a42b76a93e77dccc18bc19a560
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-11 23:19:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(array) {
+  let winCheck = array.find(el => el.result === "W")
+  return winCheck !== undefined ? winCheck.year : winCheck
+}
